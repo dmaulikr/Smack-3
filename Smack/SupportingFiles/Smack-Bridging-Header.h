32e77ea5c4b08b2283b98e259f84911db0827551
Christian Deciga <christian@everipedia.com>
2017-08-11 02:21:01 -0700

    added 2 ViewControllers set identifier segues front and rear

--- Smack/SupportingFiles/Smack-Bridging-Header.h
+++ Smack/SupportingFiles/Smack-Bridging-Header.h
@@ -2,3 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "SWRevealViewController.h" //allows us to get access of methods/functions that are programmed in objc files
