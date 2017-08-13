7d5d4d71b94e08f1d13c2efd9bc087cddf2927bd
Christian Deciga <christian@everipedia.com>
2017-08-12 19:38:05 -0700

    tested SWRevealController on simulator

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -17,7 +17,7 @@ class ChatVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
     }
 
     
