fb0c759865732b199933f6c2dda059fb775db197
Christian Deciga <christian@everipedia.com>
2017-08-14 02:21:28 -0700

    tested gradient view to ChannelVC in storyboard

--- Smack/View/GradientView.swift
+++ Smack/View/GradientView.swift
@@ -8,14 +8,36 @@
 
 import UIKit
 
+
+@IBDesignable 
 class GradientView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    //make subclass see real time action in storyboard as well as run in simulator
+    //add @IBDesignable so that it works in storyboard
+    
+    @IBInspectable var topColor: UIColor = UIColor.blue {
+        didSet {
+            self.setNeedsLayout() //update layout
+        }
     }
-    */
-
+    
+    @IBInspectable var bottomColor: UIColor = UIColor.green {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        //create layer add to UIView subclass
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds //what size it is
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    
+    
 }
