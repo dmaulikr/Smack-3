fe24810c1b5f9a743c9c3b66ccf43925e5054dfb
Christian Deciga <christian@everipedia.com>
2017-08-14 01:22:49 -0700

    created GradientView.swift file in View group

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,21 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by everipedia_iMac on 8/14/17.
+//  Copyright © 2017 Christian Deciga. All rights reserved.
+//
+
+import UIKit
+
+class GradientView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
