a71b129917df1bb55a5f74b07b7a089314524599
Christian Deciga <christian@everipedia.com>
2017-08-16 02:31:30 -0700

    created CreateAccountVC.swift, ViewController

---
+++ Smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,20 @@
+//
+//  CreateAccountVC.swift
+//  Smack
+//
+//  Created by everipedia_iMac on 8/16/17.
+//  Copyright © 2017 Christian Deciga. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by everipedia_iMac on 8/16/17.
+//  Copyright © 2017 Christian Deciga. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func closePressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
